79d012e29a78573c80b65d807fccbc35ee42105a
pavoltravnik <pavol.travnik@protonmail.com>
2021-03-02 19:53:53 +0100

    reply back

--- dbus.js
+++ dbus.js
@@ -2,15 +2,13 @@ let dbus = require("dbus-next");
 
 dbus.setBigIntCompat(true);
 
-const recipient = `${process.env.RECIPIENT}`;
-
 async function signalBot() {
   let bus = dbus.systemBus();
   let obj = await bus.getProxyObject("org.asamk.Signal", "/org/asamk/Signal");
   let iface = obj.getInterface("org.asamk.Signal");
   iface.on("MessageReceived", (timestamp, source, groupID, message, attachments) => {
     console.log("Message", timestamp, source, message, "received in group", groupID.toString("base64"));
-    iface.sendMessage(message, [], [recipient]);
+    iface.sendMessage(message, [], [source]);
   });
 }
 
